43ca52857c1c98a6dc05a55ecf748142443d457e
willdomkahari <wkahari@bancabc.co.zw>
2024-06-21 14:40:42 +0200

    removed @Repository, since it extends a repository

--- src/main/java/com/system/recipeblog/repositories/RecipeRepository.java
+++ src/main/java/com/system/recipeblog/repositories/RecipeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-@Repository
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Optional<Recipe> findByTitle(String title);
 }
